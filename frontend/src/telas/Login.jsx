e3ee82eae99f5917bdc47730b43dd54f3540415a
Felipe Soares <felipesilvasoares003@gmail.com>
2024-10-03 11:45:36 -0300

    Feito a tela de login, css global na pasta estilos (esse css é para colocar estilos que serão usados em todas, ou quase todas, telas. falta mudar a fonte do texto no tela de login (recomendo fazer no css global) e colocar o icone de olho para ver senha

--- frontend/src/telas/Login.jsx
+++ frontend/src/telas/Login.jsx
@@ -1,3 +1,33 @@
+// import React from "react";
+import Cabecalho from '../surface/Cabecalho';
+import Rodape from "../surface/Rodape";
+
+import globalStyle from "../estilos/Global.module.css"
+
 export default function Login() {
-    return <div>página de Login</div>
+    return (
+        <>
+            <Cabecalho />
+
+            <body className={globalStyle.body}>
+                <div className={globalStyle.formulario}>
+                    <h1>Usuário</h1>
+                    <input type="text" />
+
+                    <h1>Senha</h1>
+                    <input type="text" />
+                    <br />
+
+                    <a href="">Esqueci minha senha</a>
+                    <br />
+
+                    <button>Entrar</button>
+                    <br />
+
+                    <a href="">Ainda não possuo conta</a>
+                </div>
+            </body>
+            <Rodape />
+        </>
+    );
 }
