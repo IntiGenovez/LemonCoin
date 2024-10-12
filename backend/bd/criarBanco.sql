c6b88a62e1252bb308e281f6cdb911200d55b358
IntiGenovez <inti_genovez@hotmail.com>
2024-10-11 21:43:06 -0300

    criação do ddl

---
+++ backend/bd/criarBanco.sql
@@ -0,0 +1,46 @@
+create database lemoncoin;
+use lemoncoin;
+
+create table if not exists usuarios (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(45) NOT NULL,
+    email VARCHAR(45) NOT NULL,
+    senha VARCHAR(45) NOT NULL,
+    telefone VARCHAR(15) NOT NULL,
+    genero ENUM('F', 'M') NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (email),
+    UNIQUE KEY (telefone)
+);
+
+create table if not exists conta (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(45) NOT NULL,
+    saldo DECIMAL(10,2) NOT NULL,
+    fk_usuario_id INT UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (fk_usuario_id) REFERENCES usuarios (id)
+);
+
+create table if not exists categoria (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(45),
+    fk_usuario_id INT UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (fk_usuario_id) REFERENCES usuarios (id)
+);
+
+create table if not exists movimentacao (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    valor DECIMAL (10,2),
+    recorrencia ENUM('Diaria, Semanal, Mensal, Trimestral, Quadrimestral, Semestral, Anual') NOT NULL,
+    data DATETIME NOT NULL,
+    tipo ENUM('Despesa', 'Receita') NOT NULL,
+    fk_usuario_id INT UNSIGNED NOT NULL,
+    fk_conta_id INT UNSIGNED NOT NULL,
+    fk_categoria_id INT UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (fk_usuario_id) REFERENCES usuarios (id),
+    FOREIGN KEY (fk_conta_id) REFERENCES conta (id),
+    FOREIGN KEY (fk_categoria_id) REFERENCES categoria (id)
+);
