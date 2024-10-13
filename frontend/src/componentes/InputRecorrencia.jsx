fb5ff8d06e502b657bf450de2ef74ab14cac60c9
Felipe Soares <felipesilvasoares003@gmail.com>
2024-10-12 22:54:48 -0300

    Feito o layout da tela de Adicionar receita/despesa

---
+++ frontend/src/componentes/InputRecorrencia.jsx
@@ -0,0 +1,13 @@
+const InputRecorrencia = () => (
+    <select name="recorrencia" id="recorrencia">
+        <option value="diaria">Diária</option>
+        <option value="semanal">Semanal</option>
+        <option value="mensal">Mensal</option>
+        <option value="trimestral">Trimestral</option>
+        <option value="quadrimestral">Quadrimestral</option>
+        <option value="semestral">Semestral</option>
+        <option value="anual">Anual</option>
+    </select>
+)
+
+export default InputRecorrencia;
