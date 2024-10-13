2176f84ce7a5f9e6d490d71b7406616883e56b03
Felipe Soares <felipesilvasoares003@gmail.com>
2024-10-12 22:54:48 -0300

    Feito o layout da tela de Adcionar receirta/despesa

--- frontend/src/componentes/AdicionarMovimentacao.jsx
+++ frontend/src/componentes/AdicionarMovimentacao.jsx
@@ -1,24 +1,78 @@
-
 import InputAno from "./InputAno";
 import InputMes from "./InputMes";
 import InputDia from "./InputDia";
+import InputRecorrencia from "./InputRecorrencia";
+import { useState } from "react";
+
+import Styles from '../estilos/AdicionarMovimentacao.module.css';
 
 export default function AdicionarMovimentacao({ tipo }){
 
+    const [nome, setNome] = useState('');
+    const [valor, setValor] = useState('');
+    const [categoria, setCategoria] = useState('');
+    const [conta, setConta] = useState('');
+
+    const tratarInput = (setter, event) => {
+        setter(event.target.value);
+    };
+
     return(
-        <form action="">
-                <h1>{tipo}</h1>
-                <input type="text" name="nome" id="nome" />
-                <input type="text" name="valor" id="valor" />
-                <input type="text" name="categoria" id="categoria" />
-                <input type="text" name="recorrencia" id="recorrencia" />
-                <input type="text" name="conta" id="conta" />
-                <span>Data</span>
-                <InputDia />
-                <InputMes />
-                <InputAno />
-                <button>Confirmar</button>
-                <button>Cancelar</button>          
+        <form className={Styles}>
+                <div className={Styles.formulario}>
+                    <h1>Adicionar {tipo}</h1>
+
+                    <input 
+                        type="text" 
+                        name="nome" 
+                        id="nome" 
+                        placeholder="Nome: " 
+                        value={nome} 
+                        onChange={e => tratarInput(setNome, e)} 
+                    />
+
+                    <div>
+                         <input 
+                            type="text" 
+                            name="valor" 
+                            id="valor" 
+                            placeholder="Valor: R$" 
+                            value={valor} 
+                            onChange={e => tratarInput(setValor, e)} 
+                        />
+
+                        <input 
+                            type="text" 
+                            name="categoria" 
+                            id="categoria" 
+                            placeholder="Categoria: " 
+                            value={categoria} 
+                            onChange={e => tratarInput(setCategoria, e)} 
+                        />
+
+                    </div>
+                    <div>
+                        <InputRecorrencia />
+                        <input 
+                            type="text" 
+                            name="conta" 
+                            id="conta" 
+                            placeholder="Conta: " 
+                            value={conta} 
+                            onChange={e => tratarInput(setConta, e)} 
+                        />
+                    </div>
+                    <div className={Styles.divData}>
+                        <span>Data: </span>
+                        <InputDia />
+                        <InputMes />
+                        <InputAno />
+                    </div>
+                    <div className={Styles.Botoes}>
+                        <button>Confirmar</button>
+                        <button>Cancelar</button>
+                    </div>
+                </div>      
                     
         </form>
     );
