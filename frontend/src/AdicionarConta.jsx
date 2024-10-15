357f181df61538b6db217f3e64fb9136c264d99f
Felipe Soares <felipesilvasoares003@gmail.com>
2024-10-15 11:56:21 -0300

    terminei a tela de adicionar novo conta

--- frontend/src/AdicionarConta.jsx
+++
@@ -1,7 +0,0 @@
-import CrudConta from "../componentes/CrudConta"
-
-export default function AdicionarConta(){
-    return(
-        <div className="tela-padrao"><CrudConta tipo="Adicionar" /></div>
-    )
-}
