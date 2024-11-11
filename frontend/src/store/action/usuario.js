2c54e79476dc74227a2979791a82d68bd6c24e90
IntiGenovez <inti_genovez@hotmail.com>
2024-11-11 19:06:48 -0300

    login e logout funcional, validação nas telas funcional

--- frontend/src/store/action/usuario.js
+++ frontend/src/store/action/usuario.js
@@ -14,20 +14,36 @@ const userActions = {
             localStorage.setItem(userKey, JSON.stringify(usuario))
         })
     },
-    signin: (dispatch, usuario) => {
-        console.log(userKey)
-        fetch(`${urlBaseAPI}/signin`, { 
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json",
-              },
-            body: JSON.stringify(usuario),
-        })
-        .then(resp => resp.json())
-        .then(usuario => {
-            dispatch({ type: 'signin', payload: { usuario }})
-            localStorage.setItem(userKey, JSON.stringify(usuario))
-        })
+    signin: async (dispatch, usuario) => {
+        try {
+            const response = await fetch(`${urlBaseAPI}/signin`, { 
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json",
+                  },
+                body: JSON.stringify(usuario),
+            })
+
+            if (!response.ok) throw new Error("Erro de autenticação");
+
+            const data = await response.json()
+            if (data.token) {
+                dispatch({ type: 'signin', payload: { usuario: data }})
+                localStorage.setItem(userKey, JSON.stringify(data))
+
+                return true
+            } else {
+                console.error('Resposta inesperada: ', data)
+                return false
+            }
+        } catch (error) {
+            console.error("Erro no login: ", error)
+            return false
+        }
+    },
+    signout: (dispatch) => {
+        localStorage.removeItem(userKey)
+        dispatch({ type: 'signout' })
     }
 }
 
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -10,7 +10,7 @@ export const initialState = {
         email: '',
         telefone: '',
         genero: '',
-        token: ''
+        token: null
     }
 }
 
