df52e620e568db41c21ad772b3da48a64506d8db
IntiGenovez <inti_genovez@hotmail.com>
2024-11-13 19:20:12 -0300

    a tela de contas agora puxa as contas do banco de dados

--- frontend/src/telas/Contas.jsx
+++ frontend/src/telas/Contas.jsx
@@ -1,3 +1,6 @@
+import { useContext, useEffect } from 'react';
+import { DadosContexto } from '../store';
+
 import lapis from '../assets/lapis.png';
 import nubank from '../assets/nubank.png';
 import bancoDoBrasil from '../assets/Banco-do-Brasil.png';
@@ -15,19 +18,15 @@ import BotaoAdicionar from '../componentes/BotaoAdicionar'
 import styles from '../estilos/Contas.module.css'
 
 export default function Contas() {
+    const contexto = useContext(DadosContexto)
+
     return (
         <div className={styles.contas}>
             <div className={styles.container}>
-                <CardConta icone={nubank} nome="Nubank Felipe" saldo="R$ 1.000,00" />
-                <CardConta icone={itau} nome="Itaú Hebert" saldo="R$ 500,00" />
-                <CardConta icone={sicredi} nome="Sicredi João" saldo="R$ -50,00" />
-                <CardConta icone={bradesco} nome="Bradesco Clara" saldo="R$ 2.300,00" />
-                <CardConta icone={caixa} nome="Caixa José" saldo="R$ 150,00" />
-                <CardConta icone={bancoDoBrasil} nome="Banco do Brasil Laura" saldo="R$ 3.500,00" />
-                <CardConta icone={santander} nome="Santander Paulo" saldo="R$ 750,00" />
-                <CardConta icone={MercadoPago} nome="Mercado Pago André" saldo="R$ 400,00" />
-                <CardConta icone={nubank} nome="XP Investimentos Marina" saldo="R$ 5.000,00" />
-                <CardConta icone={picpay} nome="PicPay Tiago" saldo="R$ 1.800,00" /> 
+                { contexto.state.contas.map(conta => {
+                    return (<CardConta key={conta.id} icone={nubank} nome={conta.nome} saldo={conta.saldo} />)
+                })}
+                
             </div>
             <BotaoAdicionar path="/adicionar-conta" />
         </div>
