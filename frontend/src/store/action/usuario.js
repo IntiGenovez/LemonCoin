5fe8b4127c98f06d89a0b7fffaaf8afac8c82ee0
Felipe Soares <felipesilvasoares003@gmail.com>
2025-03-04 18:47:31 -0300

    cadastro de usuários está avisando caso de erro

--- frontend/src/store/action/usuario.js
+++ frontend/src/store/action/usuario.js
@@ -1,15 +1,39 @@
 import { urlBaseAPI, userKey } from "../../global"
 
 const userActions = {
-    signup: (_, usuario) => {
-        fetch(`${urlBaseAPI}/signup`, { 
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json",
-              },
-            body: JSON.stringify(usuario),
-        })
-        .then(resp => resp.json())
+    signup: async (dispatch, userData) => {
+        try {
+            console.log("enviando dados para API")
+            const response = await fetch(`${urlBaseAPI}/signup`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(userData)
+            })
+
+            let data;
+            try {
+                data = await response.json();
+            } catch (error) {
+                data = null;
+                console.error("Erro ao analisar JSON:", error);
+            }
+
+            console.log('Resposta da API:', response)
+            console.log('Dados retornados pela API:', data)
+
+            if (response.ok) {
+                dispatch({ type: 'signup', payload: data })
+                return true
+            } else {
+                console.error('Resposta inesperada: ', data)
+                return false
+            }
+        } catch (error) {
+            console.error("Erro no cadastro: ", error)
+            return false
+        }
     },
     signin: async (dispatch, usuario) => {
         try {
