1b88f719285777de01e5b392d2576528784ac6dd
IntiGenovez <inti_genovez@hotmail.com>
2025-03-29 19:17:14 -0300

    botão home

--- frontend/src/componentes/BotaoVoltar.jsx
+++ frontend/src/componentes/BotaoVoltar.jsx
@@ -1,20 +1,42 @@
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import { DadosContexto } from '../store'
 import { historyActions } from '../store/actionFirebase'
 
 import styles from '../estilos/Botao.module.css'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
 
-export default function BotaoVoltar() {
+export default function BotaoVoltar({ home }) {
     const contexto = useContext(DadosContexto)
     const navigate = useNavigate()
+    const location = useLocation()
 
     const handleClick = () => {
+        console.log(home)
+        if(home) {
+            navigate('/home')
+            return
+        }
         const historico = contexto.state.historico
         historyActions.voltarHistorico(contexto.dispatch)
         navigate(historico[historico.length - 2])
     }
+
+    const exibirBotao = () => {
+        if(home && location.pathname === '/home') 
+            return null
+
+        if(home)
+            return <button className={styles.btnVoltar} onClick={ handleClick }><i className='bx bx-home-alt-2'></i></button>
+
+        if(contexto.state.historico.length > 1) 
+            return <button className={styles.btnVoltar2} onClick={ handleClick }><i className='bx bx-chevron-left'></i></button>
+
+        return null
+    }
+
     return (
-        <button className={styles.btnVoltar} onClick={ handleClick }><i className='bx bx-chevron-left'></i></button>
+        <>
+        { exibirBotao() }
+        </>
     )
 }
