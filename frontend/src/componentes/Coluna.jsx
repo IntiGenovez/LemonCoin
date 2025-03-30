cb0408ba1681f2470a72d1e80fc5d89a32ff12b5
IntiGenovez <inti_genovez@hotmail.com>
2025-03-30 01:43:36 -0300

    relatório movimentações

--- frontend/src/componentes/Coluna.jsx
+++ frontend/src/componentes/Coluna.jsx
@@ -1,58 +1,49 @@
 import { useEffect, useState } from "react"
+
 import styles from "../estilos/Relatorio.module.css"
+import formatarValor from "../store/utils/formatCurrency"
 
 export default function Coluna({ movimentacao, cemPorcento }) {
-    const [ porcentagem, setPorcentagem ] = useState()
-    const [ mes, setMes ] = useState()
+    const [ porcentagemDespesa, setPorcentagemDespesa ] = useState()
+    const [ porcentagemReceita, setPorcentagemReceita ] = useState()
     useEffect(() => {
-        setPorcentagem(((movimentacao.saldo / cemPorcento) * 100).toFixed(2))
-        console.log(movimentacao.mes.split('-')[1])
-        switch (movimentacao.mes.split('-')[1]) {
-            case '01':
-                setMes('Janeiro')
-                break
-            case '02':
-                setMes('Fevereiro')
-                break
-            case '03':
-                setMes('Março')
-                break
-            case '04':
-                setMes('Abril')
-                break
-            case '05':
-                setMes('Maio')
-                break
-            case '06':
-                setMes('Junho')
-                break
-            case '07':
-                setMes('Julho')
-                break
-            case '08':
-                setMes('Agosto')
-                break
-            case '09':
-                setMes('Setembro')
-                break
-            case '10':
-                setMes('Outubro')
-                break
-            case '11':
-                setMes('Novembro')
-                break
-            case '12':
-                setMes('Dezembro')
-                break
-        }
+        setPorcentagemReceita(((movimentacao.valor[0] / cemPorcento) * 100).toFixed(2))
+        setPorcentagemDespesa(((movimentacao.valor[1] / cemPorcento) * 100).toFixed(2))
     }, [cemPorcento])
+
+    const handleMouseOver = e => {
+        if(e.target.firstChild.style) e.target.firstChild.style.opacity = '100%'
+    }
+
+    const handleMouseOut = e => {
+        if (e.target.firstChild.style) e.target.firstChild.style.opacity = '0%'
+      }
 
     return (
         <div className={ styles.dado }>
             <div className={ styles.coluna }>
-                <div className={ styles.colunaPreenchida } style={{ height: `${porcentagem}%` }}></div>
+                <div 
+                    className={ styles.colunaPreenchida } 
+                    style={{ height: `${porcentagemReceita}%` }}
+                    onMouseOver={ handleMouseOver }
+                    onMouseOut={ handleMouseOut }
+                >
+                    <div className={styles.tip}>{ 
+                        movimentacao.valor[0] ? 
+                            formatarValor(movimentacao.valor[0]) : null }</div>
+                </div>
+                <div
+                    className={ `${styles.colunaPreenchida} ${styles.outraCor}` }   
+                    style={{ height: `${porcentagemDespesa}%` }}
+                    onMouseOver={ handleMouseOver }
+                    onMouseOut={ handleMouseOut }
+                >
+                    <div className={styles.tip}>{ 
+                        movimentacao.valor[1] ? 
+                        formatarValor(movimentacao.valor[0]) : null }</div>
+                </div>
             </div>
-            <div className={ styles.legenda }>{ mes }/{ movimentacao.mes.split('-')[0].substr(2) }</div>
+            <div className={ styles.legenda }>{ movimentacao.mes }</div>
         </div>
     )
 }
