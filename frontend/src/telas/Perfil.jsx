ebde5506e336239c38df16a7671721a566a6f8bd
IntiGenovez <inti_genovez@hotmail.com>
2025-05-29 14:17:19 -0300

    criação de planilha de movimentações

--- frontend/src/telas/Perfil.jsx
+++ frontend/src/telas/Perfil.jsx
@@ -1,9 +1,49 @@
 
 import { DadosContexto } from '../store'
-import { useContext } from 'react'
+import { useContext, useRef } from 'react'
 import { userActions } from '../store/actionFirebase'
+import { utils, writeFileXLSX } from "xlsx";
 
 export default function Perfil() {
     const contexto = useContext(DadosContexto)
-    return (<><p onClick={ () => userActions.signout(contexto.dispatch) }>logout</p></>)
+    const tabelaRef = useRef(null)
+
+    return (
+        <>
+            <p onClick={ () => userActions.signout(contexto.dispatch) }>logout</p>
+            <p onClick={ () => {
+                console.log('teste')
+                const wb = utils.table_to_book(tabelaRef.current)
+                writeFileXLSX(wb, "SheetJSReactExport.xlsx")
+            } }>exportar Excel</p>
+            <table ref={ tabelaRef } style={{ display: 'none' }}>
+                <thead>
+                    <tr>
+                        <th>Tipo</th>
+                        <th>Valor</th>
+                        <th>Conta</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+                <tbody>{ contexto.state.movimentacoes
+                    .map((movimentacao, i) => {
+                        return (
+                            <tr key={i}>
+                                <td>{movimentacao.tipo}</td>
+                                <td>{movimentacao.valor}</td>
+                                <td>{movimentacao.conta}</td>
+                                <td>{movimentacao.categoria}</td>
+                                <td>{ 
+                                    movimentacao.data.toDate ? 
+                                    movimentacao.data.toDate().toLocaleString('pt-BR').split(',')[0] :
+                                    movimentacao.data
+                                }</td>
+                            </tr>
+                        )
+                    }) 
+                }</tbody>
+            </table>
+        </>
+    )
 }
